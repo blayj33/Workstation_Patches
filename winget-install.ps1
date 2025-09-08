7168cdbed5f0de0a30dea404386b823b949edd09
blayj33 <jblay@atlantaga.gov>
2025-09-08 09:13:02 -0400

    Update winget-install.ps1

--- winget-install.ps1
+++ winget-install.ps1
@@ -1,27 +1,7 @@
-#!ps
-#maxlength=500000
-#timeout=900000
+winget source reset --force
 
-# Install or update the Windows Package Manager (winget)
+Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.Winget.Source_8wekyb3d8bbwe
 
-if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
-    # Download the latest App Installer bundle and install winget
-    $url  = 'https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
-    $file = Join-Path $env:TEMP 'AppInstaller.msixbundle'
+winget source update
 
-    Invoke-WebRequest -Uri $url -UseBasicParsing -OutFile $file
-    Add-AppxPackage    -Path $file
-    Remove-Item        -Path $file -Force
 
-    Write-Output 'winget has been installed.'
-}
-else {
-    # If already present, update to the latest winget
-    winget upgrade `
-      --id Microsoft.DesktopAppInstaller `
-      --silent `
-      --accept-source-agreements `
-      --accept-package-agreements
-
-    Write-Output 'winget has been updated.'
-}
